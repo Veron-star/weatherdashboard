e8efd16486011d6ef0bf6ad0d13703a69b4506e9
Veron-star <64481387+Veron-star@users.noreply.github.com>
2020-06-19 10:36:10 +0800

    created javascript file

---
+++ javascript.js
@@ -0,0 +1,7 @@
+var button = document.querySelector('.button')
+var inputvalue = document.querySelector('.inputvalue')
+var name = document.querySelector('.name')
+var descrp = document.querySelector('.description')
+var temp = document.querySelector('.temp')
+
+fetch
