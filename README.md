e4f3ecb00c7c238c0b416601d516d96c509072c4
Veron-star <64481387+Veron-star@users.noreply.github.com>
2020-06-19 11:22:12 +0800

    added README file

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# weatherdashboard
+WeatherDashboard
+
+- Created weather forcast search platform using OpenWeather to retrive future, current and historical weather data.
+- 
+
+
+
+URL: https://veron-star.github.io/weatherdashboard/
+Repository: https://github.com/Veron-star/weatherdashboard
--- javascript.js
+++ javascript.js
@@ -1,13 +1,21 @@
 var button = document.querySelector('.button')
-var inputvalue = document.querySelector('.inputvalue')
+var inputValue = document.querySelector('.inputValue')
 var name = document.querySelector('.name')
 var descrp = document.querySelector('.description')
 var temp = document.querySelector('.temp')
 
 button.addEventListener('click', function(){
-    fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputvalue.value+'&appid=5591d25117b0ee81c97022c7d521410e')
+    fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputValue.value+'&appid=5591d25117b0ee81c97022c7d521410e')
     .then(response => response.json())
-    .then(data => console.log(data))
+    .then(data => {
+        var nameValue = data['name'];
+        var tempValue = data['main']['temp'];
+        var descrpValue = data['weather'][0]['description'];
+
+        name.innerHTML = nameValue;
+        temp.innerHTML = tempValue;
+        descrp.innerHTML = descrpValue;
+    })
 
     .catch(Error => alert("Invalid"))
 })
