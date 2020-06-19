619007ea9ca80d9537ed6bb7f21d198dd8ea5fde
Veron-star <64481387+Veron-star@users.noreply.github.com>
2020-06-19 10:55:47 +0800

    added API key

--- javascript.js
+++ javascript.js
@@ -4,4 +4,12 @@ var name = document.querySelector('.name')
 var descrp = document.querySelector('.description')
 var temp = document.querySelector('.temp')
 
-fetch
+button.addEventListener('click', function(){
+    fetch('http://api.openweathermap.org/data/2.5/weather?q='+inputvalue.value+'&appid=5591d25117b0ee81c97022c7d521410e')
+    .then(response => response.json())
+    .then(data => console.log(data))
+
+    .catch(Error => alert("Invalid"))
+})
+
+
