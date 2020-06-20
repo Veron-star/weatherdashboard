e04deadbcccc2b6c605eb72b38bf236383eaf2b3
Veron-star <64481387+Veron-star@users.noreply.github.com>
2020-06-20 21:53:13 +0800

    added uvindex

--- javascript.js
+++ javascript.js
@@ -1,6 +1,9 @@
 let appId = '74f6c0605d6d3bd672ec44e693f063f5';
 let units = 'metric';
 let searchMethod;
+let uvIndex;
+let lat = 'response.coord.lat';
+let lon = 'response.coord.lon';
 
 function getSearchMethod(searchTerm) {
     if(searchTerm.length === 5 && Number.parseInt(searchTerm) + '' === searchTerm)
@@ -12,6 +15,30 @@ function getSearchMethod(searchTerm) {
 function searchWeather(searchTerm) {
     getSearchMethod(searchTerm);
     fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`).then(result => {
+        return result.json();
+    }).then(result => {
+        init(result);
+    })
+}
+
+function getUvIndex(uvIndex) {
+    if (uvIndex <= 3) {
+                bgcolor = "green";
+            }
+            else if (uvIndex >= 3 || uvIndex <= 6) {
+                bgcolor = "yellow";
+            }
+            else if (uvIndex >= 6 || uvIndex <= 8) {
+                bgcolor = "orange";
+            }
+            else {
+                bgcolor = "red";
+            }
+}
+
+function searchUv(uvIndex) {
+    getUvIndex(uvIndex);
+    fetch(`https://api.openweathermap.org/data/2.5/uvi?${uvIndex}=${response}&APPID=${appId}&lat=${lat}&lon=${lon}`).then(result => {
         return result.json();
     }).then(result => {
         init(result);
@@ -50,17 +77,21 @@ function init(resultFromServer) {
     let humidityElement = document.getElementById('humidity');
     let windSpeedElement = document.getElementById('windSpeed');
     let cityHeader = document.getElementById('cityHeader');
-    let weatherIcon = document.getElementById('documentIconImg');    
+    let weatherIcon = document.getElementById('documentIconImg');
+    let uvIndexElement = document.getElementById('uvIndex');  
+    let cityDate = document.getElementById('cityDate');  
     
     weatherIcon.src = 'http://openweathermap.org/img/w/' + resultFromServer.weather[0].icon + '.png';
 
     let resultDescription = resultFromServer.weather[0].description;
     weatherDescriptionHeader.innerText = resultDescription.charAt(0).toUpperCase() + resultDescription.slice(1);
 
-    temperatureElement.innerHTML = Math.floor(resultFromServer.main.temp) + '&#176';
+    cityDate.innerHTML = moment().format("dddd, MMMM Do YYYY, h:mm a");
+    temperatureElement.innerHTML = Math.floor(resultFromServer.main.temp) + '&#176C';
     windSpeedElement.innerHTML = 'Winds at ' + Math.floor(resultFromServer.wind.speed) + ' m/s';
     cityHeader.innerHTML = resultFromServer.name;
     humidityElement.innerHTML = 'Humidity levels at ' + resultFromServer.main.humidity + '%';
+    uvIndexElement.innerHTML = 'UV Index: ';
     
     setPositionForHeaderInfo();
 }
